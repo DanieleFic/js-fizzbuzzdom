46184712d6d4296c144923456152b3115ecbc01e
DanieleFic <reb_ficus22@hotmail.it>
2021-12-10 14:41:10 +0100

    finito?

--- js/myscript.js
+++ js/myscript.js
@@ -4,16 +4,20 @@ for(let i = 0; i <= 100; i++){
 
     console.log (`interazione`, i)
     
-    scatola.innerHTML += `<div class= "box">` + i + `</div>`
+    
 
-    if((i%3 == 0) && (i != 0)){
+    if((i%3 == 0) && (i%5 == 0) && (i != 0) ){
+        scatola.innerHTML += `<div class= "box fizzbuzz">` + i + `</div>`
+    }
+    else if((i%3 == 0) && (i != 0)){ // multipo di 3 e stampiamo fizz
         scatola.innerHTML += `<div class= "box fizz">` + i + `</div>`
-    }else if ((i%5 == 0) && (i != 0)){
+
+    }else if ((i%5 == 0) && (i != 0)){ // multipo di 5 e stampiamo buzz
         scatola.innerHTML += `<div class= "box buzz ">` + i + `</div>`
-    }else if ((i%3 == 0) && (i%5 == 0) && (i != 0)){
-        scatola.innerHTML += `<div class= "box fizzbuzz ">` + i + `</div>`
+
+    }else  { // multipo di 3 e  5 e stampiamo buzz
+        scatola.innerHTML += `<div class= "box  ">` + i + `</div>`
     
 }
     
-
 }
