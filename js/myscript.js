c1c66be76e10047dedfbfc76a4a1ed2f0772a9d4
DanieleFic <reb_ficus22@hotmail.it>
2021-12-10 14:14:49 +0100

    fatta la logica, ma da aggiustare

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,21 @@
-let scatola = document.getElementById('container')
+let scatola = document.getElementById('container') 
 
 for(let i = 0; i <= 100; i++){
 
-    console.log (`interazione`, i) 
+    console.log (`interazione`, i)
+    
+    scatola.innerHTML += `<div class= "box">` + i + `</div>`
+
+    if((i%3 == 0) && (i != 0)){
+        scatola.innerHTML += `<div class= "box fizz">` + i + `</div>`
+    }else if ((i%5 == 0) && (i != 0)){
+        scatola.innerHTML += `<div class= "box buzz ">` + i + `</div>`
+    }else if ((i%3 == 0) && (i%5 == 0) && (i != 0)){
+        scatola.innerHTML += `<div class= "box fizzbuzz ">` + i + `</div>`
+    }else if ((i%3 == 0) && (i%5 == 0) && (i != 0)){
+        scatola.innerHTML += `<div class= "box fizzbuzz ">` + i + `</div>`
+
+}
+    
+
 }
