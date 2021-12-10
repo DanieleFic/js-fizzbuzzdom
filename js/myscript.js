61666e258eb20955743a7ad5e6cc0efed3df1674
DanieleFic <reb_ficus22@hotmail.it>
2021-12-10 14:44:50 +0100

    aggiunti commenti

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,12 @@
-let scatola = document.getElementById('container') 
+let scatola = document.getElementById('container') //prendo l'elemento container dove aggiungo poi  i box 
 
-for(let i = 0; i <= 100; i++){
+for(let i = 0; i <= 100; i++){ //creaiamo un ciclo for 
 
-    console.log (`interazione`, i)
+    console.log (`interazione`, i) //console log da 1-100
     
     
 
-    if((i%3 == 0) && (i%5 == 0) && (i != 0) ){
+    if((i%3 == 0) && (i%5 == 0) && (i != 0) ){ // multipo di 3 e  5 e stampiamo buzz
         scatola.innerHTML += `<div class= "box fizzbuzz">` + i + `</div>`
     }
     else if((i%3 == 0) && (i != 0)){ // multipo di 3 e stampiamo fizz
@@ -15,7 +15,7 @@ for(let i = 0; i <= 100; i++){
     }else if ((i%5 == 0) && (i != 0)){ // multipo di 5 e stampiamo buzz
         scatola.innerHTML += `<div class= "box buzz ">` + i + `</div>`
 
-    }else  { // multipo di 3 e  5 e stampiamo buzz
+    }else  { // box normale
         scatola.innerHTML += `<div class= "box  ">` + i + `</div>`
     
 }
